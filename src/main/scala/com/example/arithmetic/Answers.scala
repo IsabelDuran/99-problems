d7e902de8dc367099562cff7218df5c0487ca0bb
IsabelDuran <isabelduranchumillas@gmail.com>
2020-11-27 13:46:46 +0100

    Added gcd answers and tests

--- src/main/scala/com/example/arithmetic/Answers.scala
+++ src/main/scala/com/example/arithmetic/Answers.scala
@@ -1,7 +1,15 @@
 package com.example.arithmetic
 
 object Answers extends Exercises {
-  override def gcd(a: Int, b: Int): Int = ???
+  override def gcd(a: Int, b: Int): Int = {
+    def euclideanAlg(n: Int, m: Int) : Int = (m,n) match {
+      case _ if m > n => euclideanAlg(m, n)
+      case _ if m == 0 => n
+      case (m, n) => euclideanAlg(m, n % m)
+    }
+
+    euclideanAlg(a,b);
+  }
 
   override def isPrime(value: Int): Boolean = ???
 }
--- src/test/scala/com/example/arithmetic/ArithmeticTestSuite.scala
+++ src/test/scala/com/example/arithmetic/ArithmeticTestSuite.scala
@@ -3,4 +3,15 @@ package com.example.arithmetic
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class ArithmeticTestSuite extends AnyFlatSpec with Matchers {}
+class ArithmeticTestSuite extends AnyFlatSpec with Matchers {
+  /*"isPrime" should "return true when a number is prime" in {
+    Answers.isPrime(7) shouldEqual true
+  }
+  it should "return false when a number is not prime" in {
+    Answers.isPrime(10) shouldEqual false
+  }*/
+
+  "gcd" should "return the greater common denominator of two given numbers" in {
+    Answers.gcd(270, 192) shouldEqual 6
+  }
+}
